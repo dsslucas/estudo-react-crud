436fc02a3e6058c1c0fd0d92c00aee8bbdcce7bd
Lucas Silva <lucas_silva_souza@hotmail.com>
2021-11-05 17:16:24 -0300

    Adição de novos usuários no json através do portal Web

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -59,10 +59,59 @@ export default class UserCrud extends Component{
         return list
     }
 
+    //Atualiza os campos, para nome e email
+    updateField(event){
+        //Clona o usuário com SPREAD
+        const user = {...this.state.user}
+
+        //Usa o nome do input para procurar dentro do estado
+        user[event.target.name] = event.target.value
+
+        this.setState({user})
+    }
+
+    //JSX QUE RENDERIZA O FORMULÁRIO
+    renderForm(){
+        return (
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Nome</label>
+                            <input type="text" className="form-control"
+                                name="name" value={this.state.user.name}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o nome..."/>
+                        </div>
+                    </div>
+
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>E-mail</label>
+                            <input type="text" className="form-control"
+                                name="email" value={this.state.user.email}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o email..."/>
+                        </div>
+                    </div>
+                </div>
+
+                <hr />
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button className="btn btn-primary" onClick={e => this.save(e)}>Salvar</button>
+                        <button className="btn btn-secondary ml-2" onClick={e => this.clear(e)}>Cancelar</button>
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+
     render(){
         return (
             <Main {...headerProps}>
-                Cadastro de usuário
+                {this.renderForm()}
             </Main>
         )
     }
