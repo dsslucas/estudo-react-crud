0859bc498ad8c1c8f216a09d8d22ef21e9efcf53
Lucas Silva <lucas_silva_souza@hotmail.com>
2021-11-05 10:51:01 -0300

    Aplicação do logo e algumas alterações CSS

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,8 +1,13 @@
 import './Logo.css'
 
+//Import da imagem
+import logo from '../../assets/images/logo.png'
+
 import React from 'react'
 
 export default props => 
     <aside className="logo">
-        Logo
+        <a href="/" className="logo">
+            <img src={logo} alt="logo" />
+        </a>
     </aside>
